7ac0a7145555a1a91f489f3380232e661cf3283a
candicehawkins91 <candicehawkins91@gmail.com>
2025-01-20 14:35:13 -0600

    update js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-'use strict';
+"use strict";
 
-let weekWeather = document.getElementById('daysWeather');
-
+let weekWeather = document.getElementById("daysWeather");
 
 const daysWeather = [
   { day: "Sunday", temperature: 39, rainfall: 0 },
@@ -16,7 +15,7 @@ for (let i = 0; i < daysWeather.length; i++) {
   let temperature = dayWeather.temperature;
   let rainfall = dayWeather.rainfall;
 
-  let dayWeatherElement = document.createElement('tr');
+  let dayWeatherElement = document.createElement("tr");
   dayWeatherElement.innerHTML = `<th scope="row">${i + 1}</th>
   <td>${day}</td>
   <td>${temperature}</td>
@@ -25,4 +24,26 @@ for (let i = 0; i < daysWeather.length; i++) {
   weekWeather.appendChild(dayWeatherElement);
 }
 
+function calculateavgTemp() {
+  let avgTempP = document.getElementById("avgTempP");
+  avgTempP.innerText =
+    daysWeather.reduce((acc, curr) => acc + curr.temperature,0) /
+    daysWeather.length;
+}
 
+  let avgTempB = document.getElementById("avgTemp");
+  avgTempB.addEventListener("click",calculateavgTemp)
+
+  let maxRainB = document.getElementById("maxRain");
+
+  
+  function calculateavgTemp() {
+  let maxRainDay = document.getElementById("maxRain");
+  maxRainDay.innerText =
+  daysWeather.length;
+}
+    daysWeather.reduce((acc, curr) => acc + curr.temperature,0) /
+    
+    const maxRainP =daysWeather.reduce(maxDay, currentDay) => currentDay.rainfall > maxDay.rainfall ? currentDay : maxDay);
+    
+    document.getElementById("maxRainP").textContent = `Day with Max Rain: ${maxRainDay.day {$maxRainDay.rainfall} in`;
